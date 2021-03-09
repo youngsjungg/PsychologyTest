25112705a7b175f24cc05af2aec09bdd67f2b464
jungs <youngsjungg@gmail.com>
2021-03-09 23:57:13 +0900

    01

---
+++ app/src/main/java/com/example/psychologytest/fragment/MainFragment.kt
@@ -0,0 +1,45 @@
+package com.example.psychologytest.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.psychologytest.R
+import kotlinx.android.synthetic.main.fragment_main.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MainFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainFragment : Fragment() {
+    lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        //next버튼을 누르면 다음 페이지로 넘어가는 동작
+        btn_next.setOnClickListener {
+            navController.navigate(R.id.action_mainFragment_to_questionFragment)
+        }
+
+    }
+}
