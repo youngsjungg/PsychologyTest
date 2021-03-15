7613f5e5734eb5d240edfadac4b492feeaa04bf3
jungs <youngsjungg@gmail.com>
2021-03-15 12:05:04 +0900

    error_fix

--- app/src/main/java/com/example/psychologytest/MainActivity.kt
+++ app/src/main/java/com/example/psychologytest/MainActivity.kt
@@ -4,17 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var navController : NavController
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    
         navController = nav_host_fragment.findNavController()
+
     }
 }
